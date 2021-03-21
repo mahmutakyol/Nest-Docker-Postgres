156fb317d774d09b7a84975bb3de723fd361d6c2
Mahmut Akyol <mahmut.akyol@outlook.com.tr>
2021-03-21 11:32:45 +0300

    added pg-adminer, added global prefix

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { asap } from 'rxjs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1');
   await app.listen(3000);
 }
 bootstrap();
