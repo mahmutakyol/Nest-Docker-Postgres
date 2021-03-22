50ef84570ad11583efe6032bd1cf670c03102213
Mahmut Akyol <mahmut.akyol@outlook.com.tr>
2021-03-23 01:21:05 +0300

    Fixed cors error

--- app/src/views/auth/Login.vue
+++ app/src/views/auth/Login.vue
@@ -5,8 +5,9 @@
       <v-card width="500" class="mx-auto mt-9">
         <v-card-title>Login Area</v-card-title>
         <v-card-text>
-          <v-text-field label="Email" type="email" prepend-icon="mdi-account-circle"/>
+          <v-text-field label="Email" v-model="form.email" type="email" prepend-icon="mdi-account-circle"/>
           <v-text-field
+            v-model="form.password"
             label="Password"
             :type="showPassword ? 'text' : 'password'"
             prepend-icon="mdi-lock"
@@ -18,7 +19,7 @@
         <v-divider></v-divider>
         <v-card-actions>
           <v-btn color="success">Register</v-btn>
-          <v-btn color="info">Login</v-btn>
+          <v-btn color="info" @click="login()">Login</v-btn>
         </v-card-actions>
       </v-card>
     </v-content>
@@ -26,14 +27,27 @@
 </template>
 
 <script>
-import Navigation from '../../components/navigation.vue'
+import Navigation from '../../components/navigation'
+import rest from '../../core/rest'
 export default {
 	components: { Navigation },
   name: 'LoginComponent',
   data () {
     return {
-      showPassword: false
+      showPassword: false,
+      form: {
+        email: null,
+        password: null
+      }
     }
+  },
+  methods: {
+    login() {
+      rest.post('http://localhost:8080/v1/users/login', this.form)
+      .then((res) => {
+        console.log(res)
+      })
+    } 
   }
 }
 </script>
