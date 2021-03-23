3bb506bff3dff6db2b648b0afc84c65ba945a66d
Mahmut Akyol <mahmut.akyol@outlook.com.tr>
2021-03-23 23:18:48 +0300

    Fixed

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -2,7 +2,6 @@
   <v-main>
     <v-card width="500" class="mx-auto mt-16">
       <v-card-title>Mahmut Akyol</v-card-title>
-      <v-card-text>+90 553 364 04 74</v-card-text>
       <v-card-text>mahmut.akyol@outlook.com.tr</v-card-text>
       <v-card-text><a target="_blank" href="https://github.com/mahmutakyol">Github</a></v-card-text>
     </v-card>
